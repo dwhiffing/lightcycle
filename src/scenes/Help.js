06a15e4c222a964ccd670739e6b02be8895979ed
Daniel Whiffing <danielwhiffing@gmail.com>
2020-08-03 15:23:35 -0400

    Adds help pages

--- src/scenes/Help.js
+++ src/scenes/Help.js
@@ -1,21 +1,44 @@
+const TEXTS = [
+  'WELCOME TO LOOPZ',
+  `IN THIS GAME YOU
+MUST MAKE LOOPZ
+BY PLACING TILES
+ON THE BOARD IN
+STRATEGIC PLACES.
+YOU CAN DO IT!
+I BELIEVE IN YOU.
+YOU DID IT
+`,
+  'BY DAN WHIFFING',
+]
+
 export default class extends Phaser.Scene {
   constructor() {
     super({ key: 'Help' })
   }
 
-  init(opts) {
+  init() {
     this.input.keyboard.removeAllKeys(true)
     this.keys = this.input.keyboard.addKeys('SPACE')
-    this.keys.SPACE.on('down', this.back)
+    this.keys.SPACE.on('down', this.next)
   }
 
   create() {
-    this.add.bitmapText(32, 10, 'pixel-dan', 'HELP', 5).setOrigin(0.5)
-    this.add.bitmapText(32, 55, 'pixel-dan', 'PRESS SPACE', 5).setOrigin(0.5)
+    this.textIndex = 0
+    this.mainText = this.add
+      .bitmapText(32, 28, 'pixel-dan', TEXTS[0], 5)
+      .setCenterAlign()
+      .setOrigin(0.5)
+    this.add.bitmapText(32, 59, 'pixel-dan', 'PRESS SPACE', 5).setOrigin(0.5)
   }
 
-  back = () => {
-    this.scene.start('Menu')
+  next = () => {
+    this.textIndex++
+    if (this.textIndex < TEXTS.length) {
+      this.mainText.setText(TEXTS[this.textIndex])
+    } else {
+      this.scene.start('Menu')
+    }
   }
 
   update() {}
